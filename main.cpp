eac1b8d5a91faf5eed5c3da3baf2d98d7b65afd6
Wong Wei Jun <wong.weijun923@gmail.com>
2026-03-02 20:22:20 +0800

    Changed data structure
    Changed to using a Struct to store data and index that use unordered_map

--- main.cpp
+++ main.cpp
@@ -3,18 +3,32 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
-vector<vector<string>> readCSV(const string& filename) {
+struct SearchDB {
     vector<vector<string>> data;
+    unordered_map<string, vector<int>> artistIndex;
+    unordered_map<string, vector<int>> songIndex;
+    unordered_map<string, vector<int>> albumIndex;
+};
+
+SearchDB readCSV(const string& filename) {
+    SearchDB db;
+    vector<vector<string>> data;
+    unordered_map<string, vector<int>> artistIndex;
+    unordered_map<string, vector<int>> songIndex;
+    unordered_map<string, vector<int>> albumIndex;
+
     ifstream file(filename);
     
     if (!file.is_open()) {
         cerr << "Failed to open file: " << filename << endl;
-        return data;
+        return db;
     }
-
+    
     string line;
+    int currentLine=0;
     while (getline(file, line)) {
         vector<string> row;
         stringstream ss(line);
@@ -24,25 +38,32 @@ vector<vector<string>> readCSV(const string& filename) {
             row.push_back(cell);
         }
 
-        data.push_back(row);
-    }
+        db.data.push_back(row);
+        
+        string artist = row[2];
+        string song = row[4];
+        string album = row[3];
+        db.artistIndex[artist].push_back(currentLine);
+        db.songIndex[song].push_back(currentLine);
+        db.albumIndex[album].push_back(currentLine);
 
+        currentLine++;
+    }
+    
     file.close();
-    return data;
+    return db;
 }
 
 int main() {
-    auto data = readCSV("dataset.csv");
+    SearchDB db = readCSV("dataset.csv");
     //row[0]  row[1]    row[2]    row[3]      row[4]      row[5]      row[6]        row[7]
     //index   track_id  artists   album_name  track_name  popularity  duration_ms   explicit
     //row[8]        row[9]  row[10]   row[11]    row[12]  row[13]       row[14]       row[15]
     //danceability  energy  key       loudness   mode     speechiness   acousticness  instrumentalness
     //row[16]     row[17]   row[18]   row[19]          row[20]
     //liveness    valence   tempo     time_signature   track_genre
-    for (const auto& row : data) {
-        // for (const auto& cell : row) {
-        //     cout << cell << "\t";
-        // }
+
+    for (const auto& row : db.data) {
         if (row[2]=="Gen Hoshino") {
           for (const auto& cell : row) {
             cout << cell << "\t";
